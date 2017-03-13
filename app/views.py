8f2ea4d60f604d1e9bffe7bd5aaae7388db99295
richy0911 <seaford.bacchas@mymona.uwi.edu>
2017-03-13 06:24:46 +0000

    Create profiles views

--- app/views.py
+++ app/views.py
@@ -49,11 +49,46 @@ def profile():
 
     return render_template('profile.html', form = proform)
     
+@app.route('/profile/<userid>',methods=["GET","POST"])
+def viewprof(userid):
+    profiles={}
+    x=0
+    if request.method=='POST':
+        user= db.session.query(UserProfile).filter_by(username=userid)
+        for i in user:
+            prof={'userid':str(user.id),'username':user.username,'age':str(user.age),'first_name':user.first_name, 'last_name':user.last_name, 'gender':user.gender,'biography':user.biography, 'picture':user.filename,'profile_date':user.date_created}
+            x+=1
+        return jsonify(prof)
+    user= db.session.query(UserProfile).filter_by(username=userid)
+    return render_template('profileview.html',user=user)
 
 @app.route('/profiles/', methods= ['GET', 'POST'])
 def profiles():
     """Render the website's profiles page."""
-    return render_template('profiles.html')
+    user_profile=[]
+    p_l=[]
+    profiles={}
+    x=0
+    print request.method
+    if request.method=="POST":
+        users= db.session.query(UserProfile).all()
+        for user in users:
+            profiles={'username':user.username,'userid':str(user.id)}
+            user_profile.insert(x,profiles)
+            x+=1
+        p_up={'users':jsonify(user_profile)}
+        p_l.insert(0,p_up)
+        print x
+        return jsonify(user_profile)
+    users = db.session.query(UserProfile).all()
+    print "ABOVE"
+    print users
+    
+    for user in users:
+        user_profile.append((user.first_name, user.username))
+        print user.first_name
+        print "IN ABOVE"
+    return render_template('profiles.html', users=users) 
 
 
 ###
